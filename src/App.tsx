5c14556465bbd86257abf98199addffa49cd1446
Bernard Teye <bteye.ext@igus.net>
2026-01-12 05:50:46 +0000

    project initialization with installation of tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App(): React.JSX.Element {
+  return (
+    <>
+      <h1>Getting started with Sales Frontend</h1>
+      <p>Getting started with paragraph</p>
+    </>
+  );
+}
