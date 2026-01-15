6944df7b7f8ae87a0aedcae906835481dfc8e299
Bernard Teye <bteye.ext@igus.net>
2026-01-15 14:47:16 +0000

    add Backdrop component

---
+++ src/components/atoms/backdrop/Backdrop.tsx
@@ -0,0 +1,12 @@
+type BackdropProps = {
+  onToggle: VoidFunction;
+};
+
+export function Backdrop({ onToggle }: BackdropProps): React.JSX.Element {
+  return (
+    <div
+      onClick={onToggle}
+      className="fixed top-0 left-0 w-full h-screen backdrop-blur-sm bg-black/50"
+    />
+  );
+}
