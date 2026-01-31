4c95aee4406bca8821b1b8b6534215b1666232d6
Bernard Teye <bteye.ext@igus.net>
2026-01-31 16:37:50 +0000

    add useFetch hook

--- src/utils/hooks.utils.ts
+++ src/utils/hooks.utils.ts
@@ -1,8 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useOutsideClick(handleOutsideClick: VoidFunction) {
   useEffect(() => {
     window.addEventListener("click", handleOutsideClick);
     return () => window.removeEventListener("click", handleOutsideClick);
   }, [handleOutsideClick]);
+}
+
+export function useFetch() {
+  const [isFetching, setIsFetching] = useState(false);
+
+  return { isFetching, setIsFetching };
 }
