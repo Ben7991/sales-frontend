d97ed521f5234b4e9089dbb0c92757456b6463c9
Bernard Teye <bteye.ext@igus.net>
2026-02-01 14:31:31 +0000

    add updateProduct and updateProductImage reducers to product slice

--- src/store/slice/product/product.slice.ts
+++ src/store/slice/product/product.slice.ts
@@ -25,15 +25,40 @@ const productSlice = createSlice({
     },
     addNewProduct: (state: ProductState, action: PayloadAction<Product>) => {
       state.count = state.count + 1;
-      const updatedState = [action.payload, ...state.data];
+      const updatedData = [action.payload, ...state.data];
       if (state.data.length > 10) {
         const deleteCount = 1;
-        updatedState.splice(updatedState.length - 1, deleteCount);
+        updatedData.splice(updatedData.length - 1, deleteCount);
       }
-      state.data = [...updatedState];
+      state.data = [...updatedData];
+    },
+    updateProduct: (state: ProductState, action: PayloadAction<Product>) => {
+      const updatedData = [...state.data];
+      const existingProductIndex = updatedData.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      const deleteCount = 1;
+      updatedData.splice(existingProductIndex, deleteCount, action.payload);
+      state.data = [...updatedData];
+    },
+    updateProductImage: (
+      state: ProductState,
+      action: PayloadAction<{ id: number; imagePath: string }>,
+    ) => {
+      const updatedData = [...state.data];
+      const existingProductIndex = updatedData.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+      updatedData[existingProductIndex].imagePath = action.payload.imagePath;
+      state.data = [...updatedData];
     },
   },
 });
 
 export default productSlice.reducer;
-export const { loadProducts, addNewProduct } = productSlice.actions;
+export const {
+  loadProducts,
+  addNewProduct,
+  updateProduct,
+  updateProductImage,
+} = productSlice.actions;
