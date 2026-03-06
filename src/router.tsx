9a308eb0045fd35479d1a8b1d41f44f66f927ac5
Bernard Teye <bteye.ext@igus.net>
2026-03-06 12:06:58 +0000

    move all routes into it's own router file and refactor App.tsx to use the new router file

---
+++ src/router.tsx
@@ -0,0 +1,191 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router";
+
+import { Root } from "@/components/layouts/root/Root";
+import { Dashboard } from "@/components/layouts/dashboard/Dashboard";
+import { NotFound } from "@/pages/not-found/NotFound";
+import { CanActivate } from "./components/guards/can-activate/CanActivate";
+import { CanDeactivate } from "./components/guards/can-deactivate/CanDeactivate";
+import { ResetPasswordActivate } from "./components/guards/reset-password-activate/ResetPasswordActivate";
+import { EnsureUserHasRole } from "./components/guards/ensure-user-has-role/EnsureUserHasRole";
+
+// auth routes
+const Login = lazy(() => import("./pages/auth/login/Login"));
+const ForgotPassword = lazy(
+  () => import("./pages/auth/forgot-password/ForgotPassword"),
+);
+const ResetPassword = lazy(
+  () => import("./pages/auth/reset-password/ResetPassword"),
+);
+
+// dashboard routes
+const Overview = lazy(() => import("./pages/dashboard/overview/Overview"));
+const Supplier = lazy(() => import("./pages/dashboard/supplier/Supplier"));
+const Customer = lazy(() => import("./pages/dashboard/customer/Customer"));
+const Employee = lazy(() => import("./pages/dashboard/employee/Employee"));
+const Report = lazy(() => import("./pages/dashboard/report/Report"));
+const AddEditSupplies = lazy(
+  () => import("./pages/dashboard/purchase/add-edit-supplies/AddEditSupplies"),
+);
+const PurchaseHistory = lazy(
+  () => import("./pages/dashboard/purchase/history/PurchaseHistory"),
+);
+const PurchaseDetails = lazy(
+  () => import("./pages/dashboard/purchase/details/PurchaseDetails"),
+);
+const CategoriesProducts = lazy(
+  () => import("./pages/dashboard/categories-products/CategoriesProducts"),
+);
+const CreateOrder = lazy(
+  () => import("./pages/dashboard/sales/create-order/CreateOrder"),
+);
+const Inventory = lazy(() => import("./pages/dashboard/inventory/Inventory"));
+const OrderHistory = lazy(
+  () => import("./pages/dashboard/sales/order-history/OrderHistory"),
+);
+const Arrears = lazy(() => import("./pages/dashboard/sales/arrears/Arrears"));
+const AccountSettings = lazy(
+  () => import("./pages/dashboard/account-settings/AccountSettings"),
+);
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: (
+          <CanDeactivate>
+            <Login />
+          </CanDeactivate>
+        ),
+      },
+      {
+        path: "/forgot-password",
+        element: (
+          <CanDeactivate>
+            <ForgotPassword />
+          </CanDeactivate>
+        ),
+      },
+      {
+        path: "/reset-password",
+        element: (
+          <ResetPasswordActivate>
+            <ResetPassword />
+          </ResetPasswordActivate>
+        ),
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <CanActivate>
+            <Dashboard />
+          </CanActivate>
+        ),
+        children: [
+          { index: true, element: <Overview /> },
+          {
+            path: "/dashboard/account-settings",
+            element: <AccountSettings />,
+          },
+          {
+            path: "/dashboard/suppliers",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "PROCUREMENT_OFFICER"]}>
+                <Supplier />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/purchases",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "PROCUREMENT_OFFICER"]}>
+                <PurchaseHistory />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/purchases/add-edit",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "PROCUREMENT_OFFICER"]}>
+                <AddEditSupplies />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/purchases/:id",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "PROCUREMENT_OFFICER"]}>
+                <PurchaseDetails />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/customers",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "SALES_PERSON"]}>
+                <Customer />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/categories-products",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "PROCUREMENT_OFFICER"]}>
+                <CategoriesProducts />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/inventory",
+            element: <Inventory />,
+          },
+          {
+            path: "/dashboard/sales/add-edit",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "SALES_PERSON"]}>
+                <CreateOrder />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/sales",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "SALES_PERSON"]}>
+                <OrderHistory />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/arrears",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN", "SALES_PERSON"]}>
+                <Arrears />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/employees",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN"]}>
+                <Employee />
+              </EnsureUserHasRole>
+            ),
+          },
+          {
+            path: "/dashboard/report",
+            element: (
+              <EnsureUserHasRole roles={["ADMIN"]}>
+                <Report />
+              </EnsureUserHasRole>
+            ),
+          },
+        ],
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
